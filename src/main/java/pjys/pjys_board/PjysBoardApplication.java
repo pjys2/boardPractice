0c4e92b53bc5e2bb214e272409b0dba307625b35
Jaeyeong Park <pjys211@fasoo.com>
2025-06-15 22:12:06 +0900

    모듈 프로젝트 기본 구조 생성

---
+++ src/main/java/pjys/pjys_board/PjysBoardApplication.java
@@ -0,0 +1,13 @@
+package pjys.pjys_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjysBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjysBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pjys/pjys_board/PjysBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package pjys.pjys_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjysBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
